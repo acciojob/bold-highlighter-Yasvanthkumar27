4572ae037b69caa3d48a77279b798c5b350a019c
Yasvanth Kumar <yasvanthkumar48@gmail.com>
2025-11-25 13:24:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+
 function highlight() {
-    //Write your code here
-
-
+  const strongs = document.querySelectorAll('strong');
+  strongs.forEach(el => {
+    el.style.color = 'rgb(0, 128, 0)'; // green
+  });
 }
 
 
 function return_normal() {
-    //Write your code here
-
-    
+  const strongs = document.querySelectorAll('strong');
+  strongs.forEach(el => {
+    el.style.color = 'rgb(0, 0, 0)'; // black
+  });
 }
